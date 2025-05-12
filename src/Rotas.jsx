a56a44b5e3c0c8cd7e9bcfe4635524932d4a6aa2
PEDRO HENRIQUE AMANCIO DOS SANTOS ROSA <p.rosa@labcetec.net>
2025-05-12 18:18:57 -0300

    sim

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,32 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from './src/pages/Home';
-import DetalhesDDs from './src/pages/DetalhesDDs';
-import DetalhesEletric from "./src/pages/DetalhesEletric";
-import DetalhesMec from "./src/pages/DetalhesMec";
-import Historia from "./src/pages/Historia";
-import router from "./Rotas";
+import { Routes, Route, Router } from "react-router-dom";
+// isso funciona nessa verção? import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>,
-        children:[
-            {
-                path: 'Desenvolvimento-de-sistemas',
-                element: <DetalhesDDs/>
-            },
-            {
-                path: 'Eletronica-e-eletrotecnica',
-                element: <DetalhesEletric/>
-            },
-            {
-                path: 'Mecanica',
-                element: <DetalhesMec/>
-            },
-            {
-                path: 'Historia-do-SENAI',
-                element: <Historia/>
-            }
-        ]
-    }
-]);
+import Home from "./Pages/Home";
+
+const Rotas = () =>{
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+            </Routes>
+        </Router>
+    )
+}
+
+export default Rotas()
